8707f930e7ae1d09df903188c20523752293b4e1
Felipe Martínez Nicholls <gopimn@gmail.com>
2017-10-07 15:57:31 +0000

    Add TODO changes
    Add no connection error
    Add welcome message and fix verbose mode

--- packter.py
+++ packter.py
@@ -4,47 +4,73 @@ import pyshark
 import time, datetime, time
 import netifaces as net
 import sys, os
-parser = argparse.ArgumentParser(description='Argument parser of the packet counter script')
+
+parser = argparse.ArgumentParser(description='Python packet counter script')
 parser.add_argument('-i',action = 'store', dest = 'iface', default=1,
                    help='Network interface  to be sniffed')
 parser.add_argument('-a', action = 'store', dest = 'remoteAddr', default = 1,
                    help='Remote address')
-parser.add_argument('-d', action = 'store', dest = 'debugMode', default = 0,
-                   help='Remote address')
+parser.add_argument('-v', action = 'store', dest = 'verboseMode', default = 0,
+                   help='Show extra information with value 1')
 parser.add_argument('-t', action = 'store', dest = 'timeSecs', default = 300,
-                   help='Remote address')
+                   help='Duration of the files')
+
+
+###############           WELCOME           ###############
+print '\n###########  Welcome to packter ###########\n'
+
 args = parser.parse_args()
 captureTimeSecs=int(args.timeSecs)
-debug=int(args.debugMode)
+debug=int(args.verboseMode)
+remotead=args.remoteAddr
+gws=net.gateways()
+if gws['default'] == {}:
+    print 'No connection'
+    exit(0)
+aux=gws['default'][net.AF_INET]
+
+if args.iface == 1:
+    iface=str(aux[1])
+else:
+    iface=args.iface
+
+net.ifaddresses(iface)
+ip = net.ifaddresses(iface)[net.AF_INET][0]['addr']
+    
+print 'The interface to capture is: '+iface
+print 'The local ip is: '+ip
+if remotead != 1:
+    print 'The remote ip is: '+remotead
+if debug == 0:
+    print 'No package prints configuration'
+else:
+    print 'Will print all the packets'
+print 'Will save the files every '+str(captureTimeSecs)+' seconds'
+print 'starting in\n3'
+time.sleep(1)
+print '2'
+time.sleep(1)
+print '1'
+time.sleep(1)
+print 'GO!'
+time.sleep(1)
+##5 min GATTERING CHILD FUNCTION!
 
 def printv(string):
     global debug
     if debug == 1:
         print string
-
-#print args.iface
-# print args.remoteAddr 
-if args.iface == 1:
-    gws=net.gateways()
-    aux=gws['default'][net.AF_INET]
-    iface=str(aux[1])
-else:
-    iface=args.iface
-printv(iface)
-net.ifaddresses(iface)
-ip = net.ifaddresses(iface)[net.AF_INET][0]['addr']
-printv(ip)
-
-
-##5 min GATTERING CHILD FUNCTION!
+        
 def child():
     k=0
     n=0
     c=0
     fileName = time.strftime("%Y%m%d-%H%M%S")
     fileName = './packter'+fileName+'.txt'
-    printv(fileName)
+    print 'The actual file is: '+fileName
     f = open(fileName, 'w')
+    info = '####################### Packter File #########################'
+    f.write(info)
     #start_time = time.time()
     capture = pyshark.LiveCapture(interface=iface)
     capture.sniff(timeout= captureTimeSecs)
@@ -71,10 +97,11 @@ def child():
             printv( 'NO IP:'+str(n))
     info='#########################################################################\n'
     info = info + 'total packets on interval: ' + str(k) + '\n Total bytes on interval:' + str(c)
-    printv (info) 
+    printv (info)
+    f.write(info)
     f.flush()
     f.close()
-    printv ('File: '+ fileName + ' written succesfully')
+    print 'File: '+ fileName + ' written succesfully'
     os._exit(0)
     
 def parent():
@@ -90,8 +117,4 @@ def parent():
 parent()
 
 #TODO
-# debug mode, what should be printed and what not
-# error if no connection
-# say the functionality of the program if errors
 # signals to communicate between child and parent
-# do you want the no ip packets to be counted on the file
