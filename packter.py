524aae2e1a9b0c75d0e74e7b494a42ac650f351b
Felipe Martínez Nicholls <gopimn@gmail.com>
2017-10-12 00:49:09 +0000

    wrong commit before

--- packter.py
+++ packter.py
@@ -1,3 +1,63 @@
+#!/usr/bin/env python2
+import argparse
+import pyshark
+import time, datetime
+import netifaces as net
+import sys, os, signal
+
+parser = argparse.ArgumentParser(description='Python packet counter script using pyshark libraries')
+parser.add_argument('-i',action = 'store', dest = 'iface', default=1,
+                   help='Network interface  to be sniffed')
+parser.add_argument('-a', action = 'store', dest = 'remoteAddr', default = 1,
+                   help='Remote address')
+parser.add_argument('-v', action = 'store', dest = 'verboseMode', default = 0,
+                   help='Show extra information with value 1')
+parser.add_argument('-t', action = 'store', dest = 'timeSecs', default = 300,
+                   help='Duration of the files')
+parser.add_argument('-r', action = 'store', dest = 'header', default = 1,
+                   help='Indicates either to write the header/footer of the file or not')
+parser.add_argument('-o', action = 'store', dest = 'detailed', default = 0,
+                   help='Save detailed information file')
+
+#########################Functions###################
+def printv(string):
+    global debug
+    if debug == 1:
+        print string
+
+###############           WELCOME           ###############
+print '\n#########################Welcome to Packter#############################\n'
+print 'Starting in\n3'
+time.sleep(1)
+print '2'
+time.sleep(1)
+print '1'
+time.sleep(1)
+args = parser.parse_args()
+captureTimeSecs=int(args.timeSecs)
+debug=int(args.verboseMode)
+remotead=args.remoteAddr
+header=args.header
+detailed=args.detailed
+gws=net.gateways()
+if gws['default'] == {}:
+    print 'No connection. End of Program'
+    exit(0)
+aux=gws['default'][net.AF_INET]
+if args.iface == 1:
+    iface=str(aux[1])
+else:
+    iface=args.iface
+net.ifaddresses(iface)
+ip = net.ifaddresses(iface)[net.AF_INET][0]['addr']
+totalNoip=0
+totalCount=0
+totalBytes=0
+secCounter=0
+upBytes=0
+downBytes=0
+upCount=0
+downCount=0
 actualSec=0
 fileName = time.strftime("%Y%m%d-%H%M%S")
 fileName = 'packter'+fileName+'.txt'
@@ -84,6 +144,8 @@ if header == 1:
     info = info + 'Total packets on upload: ' + str(totalCount) + '\nTotal bytes:' + str(totalBytes)+ '\nTotal no IP:' + str(totalNoip)+'\n'
     printv (info)
     f.write(info)
+if detailed == 1:
+    f2.close()
 f.flush()
 f.close()
 printv('File: '+ fileName + ' written succesfully')
