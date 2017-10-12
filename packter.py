9f503d25344e4032aee269180ea337b52616be88
Felipe Martínez Nicholls <gopimn@gmail.com>
2017-10-12 00:29:01 +0000

    Change on the metodology of gathering the packets
    In order to be able to capture all the packets that are being send/received, last version used forks, to avoid delay between saving files. But that perspective doesn't alow us to count the up/down every sec info and send it to the main process, which could be done with a queue or by writing a file with the result(which will lead us to more dead time).
    That's why I took the decision of stop writing multiple files, and do all the gathering work before analyzing and saving the data.

--- packter.py
+++ packter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 import argparse
 import pyshark
-import time, datetime, time
+import time, datetime
 import netifaces as net
 import sys, os, signal
 
@@ -14,115 +14,98 @@ parser.add_argument('-v', action = 'store', dest = 'verboseMode', default = 0,
                    help='Show extra information with value 1')
 parser.add_argument('-t', action = 'store', dest = 'timeSecs', default = 300,
                    help='Duration of the files')
-
+#########################Functions###################
+def printv(string):
+    global debug
+    if debug == 1:
+        print string
 
 ###############           WELCOME           ###############
-print '\n###########  Welcome to packter ###########\n'
-
+print '\n#########################Welcome to Packter#############################\n'
+print 'Starting in\n3'
+time.sleep(1)
+print '2'
+time.sleep(1)
+print '1'
+time.sleep(1)
 args = parser.parse_args()
 captureTimeSecs=int(args.timeSecs)
 debug=int(args.verboseMode)
 remotead=args.remoteAddr
 gws=net.gateways()
 if gws['default'] == {}:
-    print 'No connection'
+    print 'No connection. End of Program'
     exit(0)
 aux=gws['default'][net.AF_INET]
-
 if args.iface == 1:
     iface=str(aux[1])
 else:
     iface=args.iface
-
 net.ifaddresses(iface)
 ip = net.ifaddresses(iface)[net.AF_INET][0]['addr']
-    
-print 'The interface to capture is: '+iface
-print 'The local ip is: '+ip
+totalNoip=0
+totalCount=0
+totalBytes=0
+secCounter=0
+upBytes=0
+downBytes=0
+upCount=0
+downCount=0
+actualSec=0
+fileName = time.strftime("%Y%m%d-%H%M%S")
+fileName = 'packter'+fileName+'.txt'
+f = open(fileName, 'w')
+info = '############################## Packter #################################\n'
+info = info+'The interface to capture is: '+iface + '\n'
+info = info+ 'The local ip is: '+ip+ '\n'
+info = info+'The actual file is: '+fileName+'\n'
+f.write(info)
+printv(info)
 if remotead != 1:
-    print 'The remote ip is: '+remotead
-if debug == 0:
-    print 'No package prints configuration'
-else:
-    print 'Will print all the packets'
-print 'Will save the files every '+str(captureTimeSecs)+' seconds'
-print 'starting in\n3'
-time.sleep(1)
-print '2'
-time.sleep(1)
-print '1'
-time.sleep(1)
-print 'GO!'
-time.sleep(1)
-##5 min GATTERING CHILD FUNCTION!
-def printv(string):
-    global debug
-    if debug == 1:
-        print string
-
-class GracefulKiller:
-    kill_now = False
-    def __init__(self):
-        signal.signal(signal.SIGINT, self.exit_gracefully)
-        signal.signal(signal.SIGTERM, self.exit_gracefully)
-        
-    def exit_gracefully(self,signum, frame):
-        self.kill_now = True
-        
-def child():
-    k=0
-    n=0
-    c=0
-    fileName = time.strftime("%Y%m%d-%H%M%S")
-    fileName = './packter'+fileName+'.txt'
-    print 'The actual file is: '+fileName
-    f = open(fileName, 'w')
-    info = '########################## Packter File ############################\n'
+    info = 'The remote ip is: '+remotead+ '\n'
     f.write(info)
-    #start_time = time.time()
-    capture = pyshark.LiveCapture(interface=iface)
-    capture.sniff(timeout= captureTimeSecs)
-    for i in range(0,len(capture)):
-        if 'IP' in capture[i]:
-            if  capture[i].ip.src == ip or capture[i].ip.dst == ip:
-                if args.remoteAddr != 1:
-                    if (capture[i].ip.src == args.remoteAddr or capture[i].ip.dst == args.remoteAddr):
-                        hagale= datetime.datetime.now()
-                        info=str(hagale)+':'+ capture[i].ip.src+' -> '+capture[i].ip.dst+ ' size: '+capture[i].length+' Bytes\n'
-                        printv(info)
-                        f.write(info)
-                        c= c+int (capture[i].length)
-                        k=k+1
+    printv(info)
+info ='Will save '+str(captureTimeSecs)+' seconds of data'+'\n'
+info=info+'Columns are:timeStamp,upCount,upBytes,downCount,downBytes\n'
+info = info+'############################ Packter Data ##############################\n'
+f.write(info)
+printv(info)
+capture = pyshark.LiveCapture(interface=iface)
+capture.sniff(timeout= (captureTimeSecs+1)) #one more sec just in case
+printv('Finished capture, processing data and saving results')
+for i in range(0,len(capture)):
+    if 'IP' in capture[i]:
+        if  capture[i].ip.src == ip or capture[i].ip.dst == ip:
+            if args.remoteAddr != 1:
+                if (capture[i].ip.src == args.remoteAddr or capture[i].ip.dst == args.remoteAddr):
+                    print 's' #some some
+            else:
+                totalCount=totalCount+1
+                totalBytes=totalBytes + int(capture[i].length)
+                if actualSec == 0:
+                    actualSec=capture[i].sniff_time
+                if actualSec.second != capture[i].sniff_time.second:
+                    actualText=str(actualSec.strftime("%H:%M:%S"))+','+str(upCount)+','+str(upBytes)+','+str(downCount)+','+str(downBytes)+'\n'
+                    f.write(actualText)
+                    printv(actualText)
+                    actualSec=capture[i].sniff_time
+                    upBytes=0
+                    downBytes=0
+                    upCount=0
+                    downCount=0
+                if  capture[i].ip.src == ip:#
+                    upBytes = upBytes + int(capture[i].length)
+                    upCount = upCount + 1
                 else:
-                    hagale= datetime.datetime.now()
-                    info=str(hagale)+':'+ capture[i].ip.src+' -> '+capture[i].ip.dst+ ' size: '+capture[i].length+' Bytes\n'
-                    printv (info)
-                    f.write(info)
-                    c= c+int (capture[i].length)
-                    k=k+1
-        else:
-            n=n+1
-            printv( 'NO IP:'+str(n))
-    info='#########################################################################\n'
-    info = info + 'total packets on interval: ' + str(k) + '\n Total bytes on interval:' + str(c)
-    printv (info)
-    f.write(info)
-    f.flush()
-    f.close()
-    print 'File: '+ fileName + ' written succesfully'
-    os._exit(0)
-    
-def parent():
-    killer = GracefulKiller()
-    while True:
-        printv("We are in the parent process with PID= %d"%os.getpid())
-        newRef=os.fork()
-        if newRef==0:
-            child()
-        else:
-            printv("We are in the parent process and our child process has PID= %d\n"%newRef)
-        time.sleep(captureTimeSecs)
-        if killer.kill_now:
-            break
-    print "End of the program. CTRL+C pressed.\nWill finish capturing the last file\n Bye:)"
-parent()
+                    downBytes = upBytes + int(capture[i].length)
+                    downCount = upCount + 1
+    else:
+        totalNoip=totalNoip+1
+        printv( 'NO IP:'+str(n))
+info='########################################################################\n'
+info = info + 'Total packets on upload: ' + str(totalCount) + '\nTotal bytes:' + str(totalBytes)+ '\nTotal no IP:' + str(totalNoip)+'\n'
+printv (info)
+f.write(info)
+f.flush()
+f.close()
+printv('File: '+ fileName + ' written succesfully')
