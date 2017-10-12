b14a0574b03f82b6d79a88bfdcf5b1181f8e167d
Felipe Martínez Nicholls <gopimn@gmail.com>
2017-10-12 00:39:30 +0000

    fix bug on total no ip
    wrong variable name

--- packter.py
+++ packter.py
@@ -5,7 +5,7 @@ import time, datetime
 import netifaces as net
 import sys, os, signal
 
-parser = argparse.ArgumentParser(description='Python packet counter script')
+parser = argparse.ArgumentParser(description='Python packet counter script using pyshark libraries')
 parser.add_argument('-i',action = 'store', dest = 'iface', default=1,
                    help='Network interface  to be sniffed')
 parser.add_argument('-a', action = 'store', dest = 'remoteAddr', default = 1,
@@ -14,6 +14,8 @@ parser.add_argument('-v', action = 'store', dest = 'verboseMode', default = 0,
                    help='Show extra information with value 1')
 parser.add_argument('-t', action = 'store', dest = 'timeSecs', default = 300,
                    help='Duration of the files')
+parser.add_argument('-r', action = 'store', dest = 'header', default = 1,
+                   help='Indicates either to write the header/footer of the file or not')
 #########################Functions###################
 def printv(string):
     global debug
@@ -32,6 +34,7 @@ args = parser.parse_args()
 captureTimeSecs=int(args.timeSecs)
 debug=int(args.verboseMode)
 remotead=args.remoteAddr
+header=args.header
 gws=net.gateways()
 if gws['default'] == {}:
     print 'No connection. End of Program'
@@ -55,21 +58,22 @@ actualSec=0
 fileName = time.strftime("%Y%m%d-%H%M%S")
 fileName = 'packter'+fileName+'.txt'
 f = open(fileName, 'w')
-info = '############################## Packter #################################\n'
-info = info+'The interface to capture is: '+iface + '\n'
-info = info+ 'The local ip is: '+ip+ '\n'
-info = info+'The actual file is: '+fileName+'\n'
-f.write(info)
-printv(info)
-if remotead != 1:
-    info = 'The remote ip is: '+remotead+ '\n'
+if header == 1:
+    info = '############################## Packter #################################\n'
+    info = info+'The interface to capture is: '+iface + '\n'
+    info = info+ 'The local ip is: '+ip+ '\n'
+    info = info+'The file name is: '+fileName+'\n'
     f.write(info)
     printv(info)
-info ='Will save '+str(captureTimeSecs)+' seconds of data'+'\n'
-info=info+'Columns are:timeStamp,upCount,upBytes,downCount,downBytes\n'
-info = info+'############################ Packter Data ##############################\n'
-f.write(info)
-printv(info)
+    if remotead != 1:
+        info = 'The remote ip is: '+remotead+ '\n'
+        f.write(info)
+        printv(info)
+    info ='Will save '+str(captureTimeSecs)+' seconds of data'+'\n'
+    info=info+'Columns are:timeStamp,upCount,upBytes,downCount,downBytes\n'
+    info = info+'############################ Packter Data ##############################\n'
+    f.write(info)
+    printv(info)
 capture = pyshark.LiveCapture(interface=iface)
 capture.sniff(timeout= (captureTimeSecs+1)) #one more sec just in case
 printv('Finished capture, processing data and saving results')
@@ -101,11 +105,12 @@ for i in range(0,len(capture)):
                     downCount = upCount + 1
     else:
         totalNoip=totalNoip+1
-        printv( 'NO IP:'+str(n))
-info='########################################################################\n'
-info = info + 'Total packets on upload: ' + str(totalCount) + '\nTotal bytes:' + str(totalBytes)+ '\nTotal no IP:' + str(totalNoip)+'\n'
-printv (info)
-f.write(info)
+        printv( 'NO IP:'+str(totalNoip))
+if header == 1:
+    info='########################################################################\n'
+    info = info + 'Total packets on upload: ' + str(totalCount) + '\nTotal bytes:' + str(totalBytes)+ '\nTotal no IP:' + str(totalNoip)+'\n'
+    printv (info)
+    f.write(info)
 f.flush()
 f.close()
 printv('File: '+ fileName + ' written succesfully')
